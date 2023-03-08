b0aa14e90db47da873201b02f58f881c8d11c634
rotenbirben <83558075+rotenbirben@users.noreply.github.com>
2023-03-08 14:55:24 +0100

    add comma

--- rotenbirben/settings.py
+++ rotenbirben/settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'impersonate.middleware.ImpersonateMiddleware'
+    'impersonate.middleware.ImpersonateMiddleware',
     'django.contrib.sites.middleware.CurrentSiteMiddleware'
 ]
 
